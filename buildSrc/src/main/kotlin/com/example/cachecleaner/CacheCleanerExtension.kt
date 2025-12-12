059a242aa090e1c318644586b423eab09797f16a
hanlin <994030812@qq.com>
2025-12-12 14:29:17 +0800

    Remove unused buildSrc directory and commented CacheCleaner plugin

--- buildSrc/src/main/kotlin/com/example/cachecleaner/CacheCleanerExtension.kt
+++
@@ -1,8 +0,0 @@
-//package com.example.cachecleaner
-//
-//open class CacheCleanerExtension {
-//    // 要清除的目录列表，默认为空
-//    var directories: List<String> = emptyList()
-//    // 是否在清理前打印日志，默认开启
-//    var verbose: Boolean = true
-//}
